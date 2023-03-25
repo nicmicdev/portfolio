c1ec809b8b24e4cf19737b2646224cefe3bdac6c
Niclas Michel <81393421+nicmicdev@users.noreply.github.com>
2023-03-25 21:24:55 +0100

    desktop version finished and chnanging browser title implemented

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,30 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+/*
+ * Usage
+ *   The following code will modify the title of the browser tab on the "blur" event and change it back to the original on the "focus" event.
+ *
+ */
+
+ // store the original tab title
+ var origTitle = document.title;
+
+ // function to change title when focusing on tab
+ function oldTitle() {
+   document.title = origTitle;
+ }
+
+ // function to change title when un-focusing on tab
+ function newTitle() {
+  setTimeout(() => {
+    document.title = 'Come back :)'; 
+  }, 7000);
+   
+ }
+
+ // bind functions to blur and focus events
+ window.onblur = newTitle;
+ window.onfocus = oldTitle;
