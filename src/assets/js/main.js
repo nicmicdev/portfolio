6af33dd91f22e20fc95b63813d006e826f478373
Niclas Michel <81393421+nicmicdev@users.noreply.github.com>
2023-10-25 15:07:02 +0200

    added main.js and main.css files

---
+++ src/assets/js/main.js
@@ -0,0 +1,38 @@
+// SHOW MOBILE MENU
+
+function showMenu() {
+    document.getElementById('nav-links').classList.add('mobile-menu');
+    document.getElementById('menu-icn').classList.add('d-none');
+    document.getElementById('close-icn').classList.remove('d-none');
+}
+//HIDE MOBILE MENU
+
+function hideMenu() {
+    document.getElementById('nav-links').classList.remove('mobile-menu');
+    document.getElementById('menu-icn').classList.remove('d-none');
+    document.getElementById('close-icn').classList.add('d-none');
+}
+
+//FILTER FUNTIONS FOR PROJECT SECTION
+
+function showProjects() {
+    document.getElementById('irecu').classList.add('d-none');
+    document.getElementById('join').classList.remove('d-none');
+    document.getElementById('epl').classList.remove('d-none');
+    document.getElementById('pokedex').classList.remove('d-none');
+    document.getElementById('portfolio').classList.remove('d-none');
+
+
+    console.log('!!!!show projects!!!');
+    
+}
+
+function showClient() {
+    document.getElementById('irecu').classList.remove('d-none');
+    document.getElementById('join').classList.add('d-none');
+    document.getElementById('epl').classList.add('d-none');
+    document.getElementById('pokedex').classList.add('d-none');
+    document.getElementById('portfolio').classList.add('d-none');
+
+    console.log('!!!!show client work!!!');
+}
