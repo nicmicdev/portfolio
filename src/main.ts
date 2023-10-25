d52535637193cbb560bea9f4c77069fc49445b6f
Niclas Michel <81393421+nicmicdev@users.noreply.github.com>
2023-10-25 15:06:33 +0200

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,3 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-
-// FILTER PROJECTS AND CLIENT WORK
-
-function showProjects() {
-        console.log('Showing projects');
-}
